8d626e54b3e8af312693f4ba9012afa6daff71a5
Psyduck <52734343+kedaya-blue@users.noreply.github.com>
2024-03-07 15:57:29 +0800

    Add files via upload
    线性表逆置

---
+++ 习题02/main.cpp
@@ -0,0 +1,52 @@
+﻿#include "pch.h"
+#include "iostream"
+#define Maxsize 100
+using namespace std;
+typedef struct {
+	int* data;
+	int length, size;
+}sqList;
+sqList Initlize(sqList& L) {
+	if ((L.data = new int[Maxsize]) == NULL) {
+		cout << "创建失败" << endl;
+		exit(1);
+	}
+	L.length = 0;
+	L.size = Maxsize;
+	return L;
+}
+sqList Assign(sqList& L) {
+	L.data = new int[Maxsize];
+	for (int i = 0; i < 5; i++) {
+		L.data[i] = rand();
+		L.length++;
+	}
+	return L;
+}
+sqList Revert(sqList& L) {
+	int mid;
+	for (int i = 0; i < L.length / 2; i++) {
+			mid = L.data[i];
+			L.data[i] = L.data[L.length-i-1];
+			L.data[L.length-i-1] = mid;
+		
+	}
+	return L;
+}
+int main()
+{
+	sqList L;
+	int mid;
+	Initlize(L);
+	Assign(L);
+	cout << "逆置前" << endl;
+	for (int i = 0; i < L.length; i++) {
+		cout << L.data[i]<<" ";
+	}
+	Revert(L);
+	cout <<endl << "逆置后" << endl;
+	for (int i = 0; i < L.length; i++) {
+		cout << L.data[i] << " ";
+	}
+
+}
